fc476940791de63542362b2b0cf845a6437eb4cc
jantapabh <s6035512034@phuket.psu.ac.th>
2021-01-23 16:26:10 +0700

    add commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { useState, useEffect, Component } from "react";
+import React from "react";
 import "./App.css";
 import { Course } from "./interfaces";
-import CourseItem from "./components/CourseItem";
-import NewCourseForm from "./components/NewCourseForm";
-import CoursesService  from './services/CoursesService'
 import LoginForm from "./components/LoginForm";
 import About from "./components/About";
 import CourseReview from "./components/CourseReview";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 const App = () => {
-
   return (
-    <div className="App">
-      <CourseReview />
-      <LoginForm />
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/" className="Text-underline">Home</Link></li>
+            <li><Link to="/login" className="Text-underline">Login</Link></li>
+            <li><Link to="/about" className="Text-underline">About</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login" component={LoginForm}>
+            <LoginForm />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <CourseReview />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
